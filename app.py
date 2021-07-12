8459c37a1f3ed1968dcaf9c08284646359d9f400
drime <dhruv.kandula@gmail.com>
2021-07-12 15:33:47 -0700

    rough finish

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import streamlit as st
 from newspaper import Article
 import streamlit as st
 import validators
+import numpy as np
 
 # url = "https://www.cnn.com/2021/07/12/health/us-coronavirus-monday/index.html"
 
+model = tf.keras.models.load_model("debunker_model")
 
+class_names = ["fake", "true"]
 
 def scrape_text(url):
 
@@ -28,7 +31,17 @@ def main():
 
         text = scrape_text(url)
 
-        print(text)
+        replaced_string = text.replace('"', "")
+        
+
+        st.write(replaced_string)
+
+        data = model.predict([replaced_string])
+        pred = class_names[np.argmax(data)]
+
+        st.write(pred)
+
+
 
 
 
